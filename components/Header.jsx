be7e87e108e9d75ce6ea9bc232456ce3d171ba42
Kiprotich Davis <mylesdavyz@gmail.com>
2024-04-02 08:21:35 +0300

    Creating a header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,31 @@
 import React, {useContext} from 'react'
 
+import Link from 'next/link'
+
+const categories = [{name: 'Meditation', slug: 'meditation'}, {name: 'Prayer', slug: 'prayer'}]
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='border-b w-full inline-block border-blue-400 py-8'>
+        <div className='md:float-left block'>
+          <Link href='/'>
+            <span className='cursor-pointer font-bold text-3xl text-white'>
+              GraphCMS
+            </span>
+          </Link>
+        </div>
+        <div className='hidden md:float-left md:contents'>
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span>
+                
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
