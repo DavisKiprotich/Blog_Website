521927ad44aac679886ae47a82fff5d32c0d6152
Kiprotich Davis <mylesdavyz@gmail.com>
2024-04-04 05:10:08 +0300

    Aligning my header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,10 +8,10 @@ const categories = [{name: 'Meditation', slug: 'meditation'}, {name: 'Prayer', s
 
 const Header = () => {
   return (
-    <div className='container mx-auto px-10 mb-8 mt-10 '>
+    <div className='container mx-auto px-10 mb-8 mt-20 '>
       <div className='border-b w-full border-blue-400 py-8 mt-10 flex justify-between items-center'>
-        <div className='flex items-center'>
-          <FaInstagram className='w-6 h-6 mr-2 text-blue' />
+        <div className='flex items-center space-x-6'>
+          <FaInstagram className='w-6 h-6 mr-6 text-blue' />
           <FaFacebook className='w-6 h-6 mr-10' />
           <FaTwitter className='w-6 h-6 mr-10' />
           <FaLinkedin className='w-6 h-6' />
@@ -26,8 +26,8 @@ const Header = () => {
         <div className='hidden md:float-left md:contents'>
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span>
-                
+              <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                {category.name}
               </span>
             </Link>
           ))}
