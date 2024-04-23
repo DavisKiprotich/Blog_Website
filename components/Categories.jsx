5bf25b56b6bcdfae2d45a5144eb3312f1f3ca954
Kiprotich Davis <mylesdavyz@gmail.com>
2024-04-23 19:32:55 +0300

    Working on landing page

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
+import styles from './styles/categories.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Categories = () => {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <h1 className={styles.title}>Find your favorite</h1>
+      <div className={styles.categories}>
+        <Link href='/blog?cat=style' className={`${styles.category} ${styles.style}`}>
+        <Image 
+        src='/47.jpg'
+        alt=''
+        className={styles.image}
+        width={32}
+        height={32}
+        />
+        style
+        </Link>
+        <Link href='/blog?cat=style' className={`${styles.category} ${styles.style}`}>
+        <Image 
+        src='/47.jpg'
+        alt=''
+        className={styles.image}
+        width={32}
+        height={32}
+        />
+        style
+        </Link>
+        <Link href='/blog?cat=style' className={`${styles.category} ${styles.style}`}>
+        <Image 
+        src='/47.jpg'
+        alt=''
+        className={styles.image}
+        width={32}
+        height={32}
+        />
+        style
+        </Link>
+        <Link href='/blog?cat=style' className={`${styles.category} ${styles.style}`}>
+        <Image 
+        src='/47.jpg'
+        alt=''
+        className={styles.image}
+        width={32}
+        height={32}
+        />
+        style
+        </Link>
+        <Link href='/blog?cat=style' className={`${styles.category} ${styles.style}`}>
+        <Image 
+        src='/47.jpg'
+        alt=''
+        className={styles.image}
+        width={32}
+        height={32}
+        />
+        style
+        </Link>
+      </div>  
     </div>
   )
 }
