006c848f9f1ab054c15dcd42e111c59a487b0c46
Kiprotich Davis <mylesdavyz@gmail.com>
2024-04-29 19:23:04 +0300

    Working on the side menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
       <h3 className={styles.subtitle}>{"Whats trending"}</h3>
       <h1 className={styles.title}>Most Popular</h1>
       <div className={styles.items}>
-        <div className={styles.item}>
+        <Link href='/' className={styles.item}>
           <div className={styles.imageContainer}>
             <img src="/27.jpg" alt="" fill className={styles.image} />
           </div>
@@ -15,13 +15,52 @@ const Menu = () => {
             <span className={`${styles.category} ${styles.travel}`}>
               Travel
             </span>
-            <h3 className={styles.postTitle}>jkbgkasjbjzxnm bsnmvbud</h3>
+            <h3 className={styles.postTitle}>
+              jkbgkasjbjzxnm vgfasghvjhvfhjgjvjhbgaujkgkugkjugk
+              jbajgajsvdjhgjuabsnmvbud
+            </h3>
             <div className={styles.detail}>
               <span className={styles.username}>Paul Mwas</span>
               <span className={styles.date}> - 11.04.2024</span>
             </div>
           </div>
-        </div>
+        </Link>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <img src="/27.jpg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.meditate}`}>
+              Meditate
+            </span>
+            <h3 className={styles.postTitle}>
+              jkbgkasjbjzxnm vgfasghvjhvfhjgjvjhbgaujkgkugkjugk
+              jbajgajsvdjhgjuabsnmvbud
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Paul Mwas</span>
+              <span className={styles.date}> - 11.04.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <img src="/27.jpg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.exercise}`}>
+              Exercise
+            </span>
+            <h3 className={styles.postTitle}>
+              jkbgkasjbjzxnm vgfasghvjhvfhjgjvjhbgaujkgkugkjugk
+              jbajgajsvdjhgjuabsnmvbud
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Paul Mwas</span>
+              <span className={styles.date}> - 11.04.2024</span>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
