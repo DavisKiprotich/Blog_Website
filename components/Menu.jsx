73f3a31ef0e9b95d8f62255fedc61bcbdab8d968
Kiprotich Davis <mylesdavyz@gmail.com>
2024-05-01 13:06:16 +0300

    Working on the side menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles/menu.module.css";
+import Link from "next/link";
 
 const Menu = () => {
   return (
@@ -60,6 +61,80 @@ const Menu = () => {
               <span className={styles.date}> - 11.04.2024</span>
             </div>
           </div>
+        </Link>
+      </div>
+      <h3 className={styles.subtitle}>{"Choices made by the Editor"}</h3>
+      <h1 className={styles.title}>Editor's Choicer</h1>
+      <div className={styles.items}>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <img src="/27.jpg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              jkbgkasjbjzxnm vgfasghvjhvfhjgjvjhbgaujkgkugkjugk
+              jbajgajsvdjhgjuabsnmvbud
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Paul Mwas</span>
+              <span className={styles.date}> - 11.04.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <img src="/27.jpg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.meditate}`}>
+              Meditate
+            </span>
+            <h3 className={styles.postTitle}>
+              jkbgkasjbjzxnm vgfasghvjhvfhjgjvjhbgaujkgkugkjugk
+              jbajgajsvdjhgjuabsnmvbud
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Paul Mwas</span>
+              <span className={styles.date}> - 11.04.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <img src="/27.jpg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.exercise}`}>
+              Exercise
+            </span>
+            <h3 className={styles.postTitle}>
+              jkbgkasjbjzxnm vgfasghvjhvfhjgjvjhbgaujkgkugkjugk
+              jbajgajsvdjhgjuabsnmvbud
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Paul Mwas</span>
+              <span className={styles.date}> - 11.04.2024</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <h3 className={styles.subtitle}>Make your choice</h3>
+      <h1 className={styles.title}>Categories</h1>
+      <div className={styles.categoryList}>
+        <Link href='/blog?cat=meditate' className={`${styles.category} ${styles.meditate}`}>
+          Meditate
+        </Link>
+        <Link href='/blog?cat=pray' className={`${styles.category} ${styles.pray}`}>
+          Pray
+        </Link>
+        <Link href='/blog?cat=exercise' className={`${styles.category} ${styles.exercise}`}>
+          Exercise
+        </Link>
+        <Link href='/blog?cat=travel' className={`${styles.category} ${styles.travel}`}>
+          Travel
         </Link>
       </div>
     </div>
---
+++ components/MenuPosts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuPosts = ({withImage}) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default MenuPosts
