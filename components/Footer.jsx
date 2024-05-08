bea8c111e57fcd1d1b4334c1e7aac5768461144c
Kiprotich Davis <mylesdavyz@gmail.com>
2024-05-08 07:03:03 +0300

    Finishing up the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,43 +8,44 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.logo}>
-          <Image src='/wings.jpg' alt='blog' width={50} height={50} />
-          <h2 className={styles.logoText}>Blog</h2>
+          <Image src="/wings.png" alt="blog" width={50} height={50} />
+          <h1 className={styles.logoText}>blog</h1>
         </div>
-        <p className={styles.desc}>hvahgjhzbbza\jkuhqegkvjsbcmbzdaejkhgkzdvbjcm zsjkgjvdbxc</p>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+          necessitatibus similique aspernatur obcaecati veritatis. Aperiam cum
+          porro sequi, totam minima consequuntur, aspernatur deleniti vero
+          repellendus dorales.
+        </p>
         <div className={styles.icons}>
-          <Image src='/facebook.png' alt='' width={18} height={18} />
-          <Image src='/instagram.png' alt='' width={18} height={18} />
-          <Image src='/tiktok.png' alt='' width={18} height={18} />
-          <Image src='/youtube.png' alt='' width={18} height={18} />
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
         </div>
       </div>
       <div className={styles.links}>
         <div className={styles.list}>
           <span className={styles.listTitle}>Links</span>
-          <Link href=''>Home</Link>
-          <Link href=''>Blog</Link>
-          <Link href=''>About Us</Link>
-          <Link href=''>Contact Us</Link>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
         </div>
-      </div>
-      <div className={styles.links}>
         <div className={styles.list}>
           <span className={styles.listTitle}>Tags</span>
-          <Link href=''>Meditate</Link>
-          <Link href=''>Pray</Link>
-          <Link href=''>Travel</Link>
-          <Link href=''>Style</Link>
-          <Link href=''>Exercise</Link>
+          <Link href="/">Style</Link>
+          <Link href="/">Pray</Link>
+          <Link href="/">Meditate</Link>
+          <Link href="/">Exercise</Link>
+          <Link href="/">Travel</Link>
         </div>
-      </div>
-      <div className={styles.links}>
         <div className={styles.list}>
           <span className={styles.listTitle}>Social</span>
-          <Link href=''>Facebook</Link>
-          <Link href=''>Instagram</Link>
-          <Link href=''>Tiktok</Link>
-          <Link href=''>Youtube</Link>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
         </div>
       </div>
     </div>
