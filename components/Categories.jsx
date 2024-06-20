b93506b2433822c916cae507b2a06ef71ae463ea
Kiprotich Davis <mylesdavyz@gmail.com>
2024-06-20 14:37:27 +0300

    Mapping the data dynamically in categories file

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -3,61 +3,41 @@ import styles from './styles/categories.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
-const Categories = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const Categories = async() => {
+  const data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        <Link href='/blog?cat=style' className={`${styles.category} ${styles.style}`}>
-        <Image 
-        src='/47.jpg'
-        alt=''
-        className={styles.image}
-        width={32}
-        height={32}
-        />
-        Style
-        </Link>
-        <Link href='/blog?cat=style' className={`${styles.category} ${styles.pray}`}>
-        <Image 
-        src='/earth.jpg'
-        alt=''
-        className={styles.image}
-        width={32}
-        height={32}
-        />
-        Pray
-        </Link>
-        <Link href='/blog?cat=style' className={`${styles.category} ${styles.travel}`}>
-        <Image 
-        src='/extreme-sports.jpg'
-        alt=''
-        className={styles.image}
-        width={32}
-        height={32}
-        />
-        Travel
-        </Link>
-        <Link href='/blog?cat=style' className={`${styles.category} ${styles.meditate}`}>
-        <Image 
-        src='/beach.jpg'
-        alt=''
-        className={styles.image}
-        width={32}
-        height={32}
-        />
-        Meditate
-        </Link>
-        <Link href='/blog?cat=style' className={`${styles.category} ${styles.exercise}`}>
-        <Image 
-        src='/war.jpg'
-        alt=''
-        className={styles.image}
-        width={32}
-        height={32}
-        />
-        Exercise
-        </Link>
+        {data?.map((item) =>(
+          <Link 
+          href='/blog?cat=style' 
+          className={`${styles.category} ${styles.style}`}
+          key={item._id}
+          >
+          <Image 
+          src='/47.jpg'
+          alt=''
+          className={styles.image}
+          width={32}
+          height={32}
+          />
+          Style
+          </Link>
+        ))}
+          
       </div>  
     </div>
   )
--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -9,12 +9,12 @@ export const GET = async() => {
         
         return new NextResponse(
             JSON.stringify(categories, { status:200 })
-        )
+        );
         
     } catch (error) {
         console.log(error)
         return new NextResponse(
             JSON.stringify({ message:"Something went wrong" }, { status:500 })
-        )
+        );
     }
 }
