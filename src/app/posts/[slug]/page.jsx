1f37cc6f9fc54ff0622b7df3025bffe7ff2979a4
Kiprotich Davis <mylesdavyz@gmail.com>
2024-07-03 08:43:40 +0300

    Working on posts

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,32 +1,57 @@
-import Image from 'next/image'
-import Menu from '../../../../components/Menu'
-import styles from '../../../components/styles/singlePage.module.css'
-import Comments from '../../../../components/Comments'
+import Image from "next/image";
+import Menu from "../../../../components/Menu";
+import styles from "../../../components/styles/singlePage.module.css";
+import Comments from "../../../../components/Comments";
 
-const SinglePage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+  const slug = params;
+
+  const data = await getData(slug);
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>We are buliding this project together even though it looks tiresome!</h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
+            {data?.user?.image && (
               <div className={styles.userImageContainer}>
-                <Image src='/47.jpg' alt="" fill className={styles.avatar} />
+                <Image
+                  src={data.user.image}
+                  alt=""
+                  fill
+                  className={styles.avatar}
+                />
               </div>
+            )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>Joh Doed</span>
+              <span className={styles.username}>{data?.user.name}</span>
               <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
+        {data?.img && (
           <div className={styles.imageContainer}>
-            <Image src='/47.jpg' alt="" fill className={styles.image} />
+            <Image src={data.img} alt="" fill className={styles.image} />
           </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
           <div
             className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
           />
           <div className={styles.comment}>
             <Comments />
@@ -35,7 +60,7 @@ const SinglePage = () => {
         <Menu />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SinglePage
+export default SinglePage;
