c1aedb522dc79dbda5d7d0126721ad1f958c9c40
Kiprotich Davis <mylesdavyz@gmail.com>
2024-07-06 11:13:34 +0300

    Functionality of comments section

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -20,20 +20,37 @@ const fetcher = async (url) => {
 };
 
 const Comments = ({ postSlug }) => {
-  const status = useSession();
+  const { status } = useSession();
 
-  const { data, isLoading } = useSWR(
+  const { data, mutate, isLoading } = useSWR(
     `http://localhost:3000/api/comments?postSlug=${postSlug}`,
     fetcher
   );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () =>{
+    await fetch(`http://localhost:3000/api/comments`, {
+    method: "POST",
+    body: JSON.stringify({
+      desc,
+      postSlug,
+    }),
+  });  
+  mutate();  
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea placeholder="write a comment..." className={styles.input} />
-          <button className={styles.button}>Send</button>
+          <textarea
+            placeholder="write a comment..."
+            className={styles.input}
+            onChange={(e = setDesc(e.target.value))}
+          />
+          <button className={styles.button} onclick={handleSubmit}>Send</button>
         </div>
       ) : (
         <Link href="/login">Login to write a comment</Link>
@@ -44,21 +61,21 @@ const Comments = ({ postSlug }) => {
           : data?.map((item) => (
               <div className={styles.comment} key={item._id}>
                 <div className={styles.user}>
-                  {item?.user?.image && <Image
-                    src={item.user.image}
-                    alt=""
-                    width={50}
-                    height={50}
-                    className={styles.image}
-                  />}
+                  {item?.user?.image && (
+                    <Image
+                      src={item.user.image}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
                   <div className={styles.userInfo}>
                     <span className={styles.username}>{item.user.name}</span>
                     <span className={styles.date}>{item.createdAt}</span>
                   </div>
                 </div>
-                <p className={styles.desc}>
-                  {item.desc}
-                </p>
+                <p className={styles.desc}>{item.desc}</p>
               </div>
             ))}
       </div>
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,9 +11,14 @@ const authOptions =  {
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    GithubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+    }),
 ],
 }
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET , handler as POST }
+export const getAuthSession = () => getServerSession(authOptions)
