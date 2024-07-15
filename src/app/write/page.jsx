fe4dbe82571c9433e9ae29fe9097f800be7227e2
Kiprotich Davis <mylesdavyz@gmail.com>
2024-07-15 15:55:05 +0300

    Modify writePage

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -17,15 +17,14 @@ import { app } from "@/utilities/firebase";
 
 const WritePage = () => {
   const { status } = useSession();
-
-  const router = useRouter()
-
+  const router = useRouter();
 
   const [open, setOpen] = useState(false);
   const [file, setFile] = useState(null);
   const [media, setMedia] = useState("");
-  const [value, setValue] = useState('');
-  const [title, setTitle] = useState('');
+  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [catSlug, setCatSlug] = useState("");
 
   useEffect(() => {
     const storage = getStorage(app);
@@ -61,14 +60,15 @@ const WritePage = () => {
 
     file && upload();
   }, [file]);
-  
-  if(status === "loading"){
-    return <div className={styles.loading}>Loading....</div>
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
   }
 
-  if(status === "unauthenticated"){
-    router.push("/")
+  if (status === "unauthenticated") {
+    router.push("/");
   }
+
   const slugify = (str) =>
     str
       .toLowerCase()
@@ -76,7 +76,6 @@ const WritePage = () => {
       .replace(/[^\w\s-]/g, "")
       .replace(/[\s_-]+/g, "-")
       .replace(/^-+|-+$/g, "");
-
 
   const handleSubmit = async () => {
     const res = await fetch("/api/posts", {
@@ -88,20 +87,37 @@ const WritePage = () => {
         slug: slugify(title),
         catSlug: catSlug || "style", //If not selected, choose the general category
       }),
-    })
-    console.log(res)
+    });
+
+    if (res.status === 200) {
+      const data = await res.json();
+      router.push(`/posts/${data.slug}`);
+    }
   };
 
   return (
     <div className={styles.container}>
       <input type="text" placeholder="Title" className={styles.input} onChange={e => setTitle(e.target.value)} />
+      <select className={styles.select} onChange={(e) => setCatSlug(e.target.value)}>
+        <option value="style">style</option>
+        <option value="fashion">fashion</option>
+        <option value="pray">Prayer</option>
+        <option value="culture">culture</option>
+        <option value="travel">travel</option>
+        <option value="coding">coding</option>
+      </select>
       <div className={styles.editor}>
         <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
           <div className={styles.add}>
-            <input type="file" id="image" onChange={e=>setFile(e.target.files[0])} className={{display: "none"}} />
+            <input
+              type="file"
+              id="image"
+              onChange={(e) => setFile(e.target.files[0])}
+              style={{ display: "none" }}
+            />
             <button className={styles.addButton}>
               <label htmlFor="image">
                 <Image src="/image.png" alt="" width={16} height={16} />
@@ -115,10 +131,17 @@ const WritePage = () => {
             </button>
           </div>
         )}
-        <ReactQuill className={styles.textArea} theme="bubble" placeholder="Write your story..." value={value} onChange={setValue}/>
-
+        <ReactQuill
+          className={styles.textArea}
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..."
+        />
       </div>
-      <button className={styles.publish} onClick={handleSubmit}>Publish</button>
+      <button className={styles.publish} onClick={handleSubmit}>
+        Publish
+      </button>
     </div>
   );
 };
