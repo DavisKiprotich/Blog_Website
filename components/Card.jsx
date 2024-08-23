515a4923c2c6d05170bca43d847ef68396bffb9a
Davis Kiprotich <100116526+DavisKiprotich@users.noreply.github.com>
2024-08-23 16:10:50 +0300

    Update Card.jsx
    Replaced a paragraph tag used for the description in the card with a div tag

--- components/Card.jsx
+++ components/Card.jsx
@@ -20,9 +20,10 @@ const Card = ({ key, item }) => {
         <Link href={`/posts/${item.slug}`}>
           <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
+    {/*  <p className={styles.desc}>
           {item.desc.substring(0, 10)}
-        </p>
+        </p> */}
+        <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0,60) }}/>
         <Link href={`/posts/${item.slug}`} className={styles.link}>
           Read More
         </Link>
